83e20c03485a8c6eea3b15acdc099b7adf136c7f
Rinor Rama <rinor.rama95@gmail.com>
2022-02-10 17:19:05 +0100

    bugfix

--- src/componets/QuotesList.tsx
+++ src/componets/QuotesList.tsx
@@ -7,7 +7,7 @@ function QuotesList({ quotes }: Props) {
   return (
     <ul className="quotes__list">
       {quotes.map((quote) => (
-        <li className="quotes__list-item">
+        <li className="quotes__list-item" key={quote.id}>
           <Quote quote={quote} />
         </li>
       ))}
